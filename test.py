22758df7ce810ae51a96705c726959dfaa0d7714
Scarykiller <pierrenicolasp@gmail.com>
2018-04-18 15:58:07 +0200

    DFFferrfg

--- test.py
+++ test.py
@@ -1,22 +1,45 @@
 import xlrd
+import re
 from docx import Document
 
+numberLines=0;
 
+document = Document(r"C:\Users\ppintus\Documents\csd\courrier.docx")
 
-from docx import Document
-from docx.shared import Inches
-document = Document("D:\Documents\CSD\  \testWord")
-
-
-wb = xlrd.open_workbook("D:\Documents\CSD\\test.xls");
+wb = xlrd.open_workbook(r"C:\Users\ppintus\Documents\csd\\testexcel.xls");
 print(wb.sheet_names());
-sh= wb.sheet_by_name('Feuille1')
-for rownum in range(sh.nrows):
-    print(sh.row_values(rownum));
+sh= wb.sheet_by_name('Feuil1')
+for rownum in range(1,sh.nrows):
+    a=(sh.row_values(rownum));
+    print(a[0])
+    print(a[1])
+    print(a[2])
 
-colonne1 = (sh.row_values(1));
+replace1=re.compile((sh.row_values(1)[0]).toString());
+#CHANGER EN STRING
+
+regex=r"champ1";
+document.save(r"C:\Users\ppintus\Documents\csd\courrier.docx")
 
 
-print(colonne1);
-print(colonne1[0]);
 
+
+def docx_replace_regex(doc_obj, regex , replace):
+
+    for p in doc_obj.paragraphs:
+        if regex.search(p.text):
+            inline = p.runs
+            # Loop added to work with runs (strings with same style)
+            for i in range(len(inline)):
+                if regex.search(inline[i].text):
+                    text = regex.sub(replace, inline[i].text)
+                    inline[i].text = text
+
+    for table in doc_obj.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                docx_replace_regex(cell, regex , replace)
+
+regex1 = re.compile(r"Caverne")
+replace1 = r"Monsieur le président"
+docx_replace_regex(document, regex1 , replace1)
